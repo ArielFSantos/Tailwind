d64941b075eff82e5d635bef6c6985c470f29a9a
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-08 21:14:19 -0300

    study
    aprendi mas nao precisei executar muito

---
+++ dia01/estudo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>  
+      <body class="flex items-center justify-center h-screen bg-blue-500" >
+        <main class="bg-white p-40 rounded shadow-md"></main>
+      </body>
+
+    </>
+  )
+}
+
+export default App
